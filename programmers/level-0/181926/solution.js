47856686a53081636e0000b9c7a81505338767ec
mynolog <minholee.log@gmail.com>
2025-02-27 23:51:12 +0900

    programmers 181926: 수 조작하기 1(코딩 기초 트레이닝)

---
+++ programmers/level-0/181926/solution.js
@@ -0,0 +1,20 @@
+function solution(n, control) {
+  var answer = n;
+  [...control].forEach((char) => {
+    switch (char) {
+      case "w":
+        answer++;
+        break;
+      case "s":
+        answer--;
+        break;
+      case "d":
+        answer += 10;
+        break;
+      case "a":
+        answer -= 10;
+        break;
+    }
+  });
+  return answer;
+}
