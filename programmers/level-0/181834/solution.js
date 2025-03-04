0fd1d94c041f4a49447684f9d3792f6d747074d9
mynolog <minholee.log@gmail.com>
2025-03-04 22:26:36 +0900

    programmers 181834: l로 만들기(코딩 기초 트레이닝)

--- programmers/level-0/181834/solution.js
+++ programmers/level-0/181834/solution.js
@@ -1,8 +1,10 @@
 function solution(myString) {
   return [...myString]
     .map((char) => {
+      // 108보다 작으면 "l" 반환
       if (char.charCodeAt() < 108) {
         return "l";
+        // 108보다 크거나 같으면 문자열 그대로 반환
       } else {
         return char;
       }
